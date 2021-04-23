ad95b4c82dd70194e3cc862b92bfdd589edb596f
lushenao09201521 <1213756681@qq.com>
2021-04-23 14:05:04 +0800

    接口工程

---
+++ src/main/java/com/ch/springboot/model/User2.java
@@ -0,0 +1,55 @@
+package com.ch.springboot.model;
+
+import java.io.Serializable;
+
+public class User2 implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private String age;
+
+    private String sex;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
---
+++ src/main/java/com/ch/springboot/service/UserService.java
@@ -0,0 +1,8 @@
+package com.ch.springboot.service;
+
+import com.ch.springboot.model.User2;
+
+public interface UserService {
+
+   User2 querUserId(Integer id);
+}
